453baebf2197bfe95d867e0879576bd1e69fb550
joannavarrete <joan.navarrete.sanchez@gmail.com>
2023-01-16 17:18:51 +0100

    bug fix

--- src/main/java/com/joannava/kafka/katas/producers/TransactionProducer.java
+++ src/main/java/com/joannava/kafka/katas/producers/TransactionProducer.java
@@ -1,15 +1,16 @@
 package com.joannava.kafka.katas.producers;
 
+import static com.joannava.kafka.katas.utils.PropertiesUtils.getProperties;
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.KeyValue;
-
-import static com.joannava.kafka.katas.utils.PropertiesUtils.*;
-import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
 
 public class TransactionProducer {
 
@@ -22,7 +23,7 @@ public class TransactionProducer {
         // create Producer Properties
         Properties properties = new Properties();
         properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, appProperties.getProperty(BOOTSTRAP_SERVERS_CONFIG));
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         producer = new KafkaProducer<>(properties);
     }
